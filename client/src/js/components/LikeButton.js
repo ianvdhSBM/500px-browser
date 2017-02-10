6a44c3413a700648ba02f88a141eddd0d4d8e5f9
Ian Van Den Heuvel <ianvdh@gmail.com>
2017-02-10 13:53:57 -0500

    Adjust css and svg sizes.

--- client/src/js/components/LikeButton.js
+++ client/src/js/components/LikeButton.js
@@ -37,7 +37,14 @@ export default class LikeButton extends React.Component {
 
 
     return <div className="like-button">
-      <svg onClick={this.handleLike} className={svgClassName} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <svg
+        onClick={this.handleLike}
+        className={svgClassName}
+        xmlns="http://www.w3.org/2000/svg"
+        width="25" 
+        height="25" 
+        viewBox="0 0 24 24"
+      >
         <path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/>
       </svg>
     </div>;
