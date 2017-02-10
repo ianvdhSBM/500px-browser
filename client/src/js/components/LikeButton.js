e4864afb09be98c161286f0f04acc017a5ad7a26
Ian Van Den Heuvel <ianvdh@gmail.com>
2017-02-10 14:09:32 -0500

    Update README and some prototypes.

--- client/src/js/components/LikeButton.js
+++ client/src/js/components/LikeButton.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 
 export default class LikeButton extends React.Component {
   static propTypes = {
-    authenticated: PropTypes.bool,
+    authenticated: PropTypes.bool.isRequired,
     handleLikePhotoRequest: PropTypes.func.isRequired,
     photoId: PropTypes.number.isRequired,
   }
@@ -41,8 +41,8 @@ export default class LikeButton extends React.Component {
         onClick={this.handleLike}
         className={svgClassName}
         xmlns="http://www.w3.org/2000/svg"
-        width="25" 
-        height="25" 
+        width="25"
+        height="25"
         viewBox="0 0 24 24"
       >
         <path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/>
